1f691ae130b36f66ec4d47ac6b9dce2e135d065c
icecoast-ski <rickt123@gmail.com>
2026-02-08 18:15:02 -0500

    Add files via upload

--- manual-data.js
+++ manual-data.js
@@ -3,6 +3,24 @@
  * Generated from admin.html
  */
 window.MANUAL_RESORT_OVERRIDES = {
+  "jay-peak": {
+    "conditions": "Powder",
+    "icecoastRating": 1,
+    "liftTicket": {
+      "weekday": "$99",
+      "weekend": "$119"
+    },
+    "parking": "Free",
+    "dynamicPricing": false,
+    "trails": {
+      "open": 82
+    },
+    "lifts": {
+      "total": 9
+    },
+    "snowfall48h": 4,
+    "snowfall7d": 12
+  },
   "killington": {
     "conditions": "Machine Groomed",
     "icecoastRating": 4,
@@ -21,5 +39,99 @@ window.MANUAL_RESORT_OVERRIDES = {
     "snowfall24h": 0,
     "snowfall48h": 6,
     "snowfall7d": 9
+  },
+  "mount-snow": {
+    "conditions": "Machine Groomed",
+    "icecoastRating": 4,
+    "liftTicket": {
+      "weekday": "$99+",
+      "weekend": "$115+"
+    },
+    "parking": "Free",
+    "dynamicPricing": true,
+    "trails": {
+      "open": 68
+    },
+    "lifts": {
+      "total": 20
+    },
+    "snowfall24h": 0,
+    "snowfall48h": 1,
+    "snowfall7d": 2
+  },
+  "okemo": {
+    "conditions": "Machine Groomed",
+    "icecoastRating": 4,
+    "liftTicket": {
+      "weekday": "$99+",
+      "weekend": "$125+"
+    },
+    "parking": "Free",
+    "dynamicPricing": false,
+    "trails": {
+      "open": 115
+    },
+    "lifts": {
+      "total": 20
+    },
+    "snowfall24h": 1,
+    "snowfall48h": 2,
+    "snowfall7d": 2
+  },
+  "stowe": {
+    "conditions": "Variable",
+    "liftTicket": {
+      "weekday": "$125+",
+      "weekend": "$169+"
+    },
+    "parking": "Free",
+    "dynamicPricing": true,
+    "trails": {
+      "open": 120
+    },
+    "lifts": {
+      "total": 12
+    },
+    "snowfall24h": 2,
+    "snowfall48h": 7,
+    "snowfall7d": 13
+  },
+  "stratton": {
+    "conditions": "Machine Groomed",
+    "icecoastRating": 4,
+    "liftTicket": {
+      "weekday": "$88+",
+      "weekend": "$146+"
+    },
+    "parking": "Free",
+    "dynamicPricing": true,
+    "trails": {
+      "open": 98,
+      "closed": 1
+    },
+    "lifts": {
+      "total": 14
+    },
+    "snowfall24h": 0,
+    "snowfall48h": 3
+  },
+  "sugarbush": {
+    "conditions": "Packed Powder",
+    "icecoastRating": 4,
+    "liftTicket": {
+      "weekday": "$129+",
+      "weekend": "$179+"
+    },
+    "parking": "Free",
+    "dynamicPricing": true,
+    "trails": {
+      "open": 111
+    },
+    "lifts": {
+      "total": 16
+    },
+    "snowfall24h": 2,
+    "snowfall48h": 6,
+    "snowfall7d": 7
   }
 };
