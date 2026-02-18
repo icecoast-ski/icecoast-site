453469f9d33d9563526bf0397e0b8ba2d5d0bcf8
icecoast-ski <rickt123@gmail.com>
2026-02-18 15:21:14 -0500

    Add files via upload

---
+++ resort-source/manual-data.js
@@ -0,0 +1,25 @@
+/*
+ * ICECOAST manual resort overrides
+ * Generated from admin.html
+ */
+window.MANUAL_RESORT_OVERRIDES = {
+  "killington": {
+    "conditions": "Machine Groomed",
+    "icecoastRating": 4,
+    "liftTicket": {
+      "weekday": "$157+",
+      "weekend": "$172+"
+    },
+    "parking": "Free",
+    "dynamicPricing": true,
+    "trails": {
+      "open": 96
+    },
+    "lifts": {
+      "total": 21
+    },
+    "snowfall24h": 0,
+    "snowfall48h": 6,
+    "snowfall7d": 9
+  }
+};
